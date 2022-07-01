a0fb0ee2a1e78eb90eb7ed060c2d6e4a09984640
Relaxe <thistletealeaves@gmail.com>
2022-07-01 07:14:58 -0400

    Chapter 4 completed.

--- Chapter_4-Comments/Chapter_4-Comments/Program.cs
+++ Chapter_4-Comments/Chapter_4-Comments/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+String thingSubject, //User inputs subject from text box
+       thingDescribed, //User inputs subject adjective from text box
+       thing3 = "of Doom", 
+       thing4 = "3000"; 
+
+Console.WriteLine("What kind of thing are we talking about?"); 
+thingSubject = Console.ReadLine(); //Obtain noun from user
+Console.WriteLine("How would you describe the thing? Big? Azure? Tattered?");
+thingDescribed = Console.ReadLine(); //Obtain adjective from user
+
+// Assemble user input
+Console.WriteLine("You must be talking about the " + thingSubject + " " + thingDescribed + " " + thing3 + " " + thing4 + "!");
