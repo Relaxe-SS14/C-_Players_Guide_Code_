86ceb60b7675c5e046e9fd12f149d2fb19a6b782
Relaxe <thistletealeaves@gmail.com>
2022-07-01 06:56:10 -0400

    Chapter 3 Completed.

--- Hello_World/Hello_World/Hello_World/Program.cs
+++ Hello_World/Hello_World/Hello_World/Program.cs
@@ -1,2 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+//Mission 1: Write a line to the console!
+
+/*
 Console.WriteLine("Hello, World!");
+*/
+
+//Mission 2: Make the program write more than one line to the console.
+
+/*
+Console.WriteLine("Literal Variables are hard coded into a program when they aren't declared as variables.");
+Console.WriteLine("A program can have a nearly infinite amount of statements, which is a single direction terminated by ;");
+Console.WriteLine("It appears that the program automatically terminates (or adds a new line to the console) when you finish .WriteLine.");
+Console.WriteLine("This is the end of our program.");
+*/
+
+
+
+
+//Mission 3: Declare a variable, and use it as a container for a users input. Write that variable to the console.
+string userInput;
+Console.WriteLine("Who recieves the bread??");
+userInput = Console.ReadLine();
+Console.WriteLine(userInput + " recieves the bread.");
